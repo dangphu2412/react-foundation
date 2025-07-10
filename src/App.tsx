03cb344b718353033891f7bbddefb59474e3c24c
dangphu2412 <dangphu241299@gmail.com>
2025-07-10 14:36:27 +0700

    feat: build posts page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,42 @@
 import './App.css'
+import {type FC, useEffect, useState} from "react";
+import {Button} from "@/components/ui/button.tsx";
+import {PostsPage} from "@/modules/posts/PostsPage.tsx";
+
+type Props = {
+    children: FC<{ path: string }>;
+}
+
+function Router({ children }: Props) {
+    const [path, setPath] = useState(window.location.pathname);
+
+    useEffect(() => {
+        function onPopStateChange() {
+            setPath(window.location.pathname)
+        }
+
+        window.addEventListener('popstate', onPopStateChange)
+
+
+        return () => {
+            window.removeEventListener('popstate', onPopStateChange)
+        }
+    }, []);
+
+    return <>{children({ path })}</>
+}
 
 function App() {
   return (
-    <>
-      Hello world
-    </>
+    <Router>
+        {({ path }) => {
+            if (path === '/posts/add') {
+                return <Button>Add</Button>
+            }
+
+            return <PostsPage />
+        }}
+    </Router>
   )
 }
 
